4129b56c63b459f732acd458275579fdf3277fc7
marian4q <marian4q@argentina.com>
2015-04-12 11:33:23 -0300

    Sumatoria

--- js/app.js
+++ js/app.js
@@ -1,13 +1,18 @@
 (function(){
+	var itemsPriceArr = [];
+	var totalPrice = 0;
 
 	console.log("MAr");
 	var numItems = prompt("Ingrese la Cantidad de Items");
 
-	for (i=1; i<=numItems; i++){
-		var itemPrice[i] = prompt("Ingrese Costo Item "+ i);
+	for (var i=1; i<=numItems; i++){
+		itemPrice = parseFloat(prompt("Ingrese Costo Item "+ i));
+		totalPrice = (totalPrice+itemPrice);
+		itemsPriceArr.push(itemPrice);
+
 	}
-	console.log(itemPrice)
-	
+	console.log(itemsPriceArr);
+	console.log(totalPrice) ;
 	
 	// var totalPrice = prompt("Ingrese Costo total");
 	// $(".total-price").text(totalPrice);
