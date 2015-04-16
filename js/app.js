dbc82706cb04edb2b9f85c9eb45f9cfc19d49ceb
MarianQ <marian4q@argentina.com>
2015-04-16 01:04:12 -0300

    glb update

--- js/app.js
+++ js/app.js
@@ -17,33 +17,59 @@
 	// var totalPrice = prompt("Ingrese Costo total");
 	// $(".total-price").text(totalPrice);
 
+        var content = "";
+        var soldByAmazon = "Sold by: Amazon.com LLC";
+        var conditionNew = "Condition: New";
+        var conditionUSed = "Condition: Used";
+
+        var itemDesc = prompt("Insert Item description");
+
+        //BR
+        var brEl = document.createElement ("BR");
 
 	//TR Row
-var tabTr = document.createElement ("TR");
-tabTr.id = "item-desc";
+        var tabTr = document.createElement ("TR");
+        tabTr.id = "item-desc";
 
-//TD left Cell
-var tabTd = document.createElement("TD")
-    tabTd.setAttribute("colspan", "1");
-    tabTd.setAttribute("valign", "top");
-    content = document.createTextNode("1 of:");
+        //TD left Cell
+        var tabTd = document.createElement("TD")
+            tabTd.setAttribute("colspan", "1");
+            tabTd.setAttribute("valign", "top");
+            
+            content = document.createTextNode("1 of:");
+            tabTd.appendChild(content);
 
-    tabTd.appendChild(content);
+        // List Element I
+        var tabI = document.createElement("I");
+            tabI.setAttribute("class", "item one");
 
-// List Element I
-var tabI = document.createElement("I");
-    tabI.setAttribute("class", "item one");
+            content = document.createTextNode(itemDesc);
+            tabI.appendChild(content);
+            
+        // Span small
+        var spanCond = document.createElement("span");
+            spanCond.setAttribute("class", "tiny");
+            
+            content = document.createTextNode(soldByAmazon);
+            spanCond.appendChild(content);
+                      
+        // Span small 2   
+        var spanCond2 = document.createElement("span");
+            spanCond.setAttribute("class", "tiny");
+            content = document.createTextNode(conditionNew);
+            spanCond2.appendChild(content);
+        
+        //Elements Creation
+        
+        tabTd.appendChild(tabI);
+        tabTd.appendChild(brEl);
+        tabTd.appendChild(spanCond);
+        tabTd.appendChild(brEl);
+        tabTd.appendChild(spanCond2);
+        tabTd.appendChild(brEl);
+        tabTr.appendChild(tabTd);
 
-    content = document.createTextNode(itemDesc);
-    tabI.appendChild(content);
-    tabI.appendChild(brEl);
-    tabI.appendChild(brEl);
 
-
-tabTr.appendChild(tabTd);
-tabTd.appendChild(tabI);
-
-
-var a = document.getElementById("table-items-body");
-a.appendChild(tabTr);
+        var a = document.getElementById("table-items-body");
+        a.appendChild(tabTr);
 })();
